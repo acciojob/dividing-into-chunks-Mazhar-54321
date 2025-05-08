41581ff51bb7f1fe289be7bf1f0634a138bf251e
Mazhar-54321 <160312563+Mazhar-54321@users.noreply.github.com>
2025-05-08 06:29:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,34 @@
 const arr = [1, 2, 3, 4, 1, 0, 2, 2];
 
 const divide = (arr, n) => {
-  // Write your code here
+  const chunks=[];
+  for(let i=0;i<arr.length;i++){
+	//console.log(i)
+        let sum = arr[i];
+		let res=[sum]
+		if(sum>n){
+			continue;
+		}
+        // console.log(res,"mklll")
+		for(let j=i+1,c=0;j<arr.length;j++,c++){
+           sum+=arr[j];
+		   //console.log(sum,j)
+		   if(sum<=n){
+			res.push(arr[j]);
+			if(j === arr.length-1){
+				console.log("end",i,c)
+				i+=c+1;
+			}
+		   }else{
+			i+=c;
+			break;
+		   }
+		}
+		//console.log(i,"mmm")
+		chunks.push(res)
+  }
+  //console.log(chunks)
+  return chunks
 };
 
 const n = prompt("Enter n: ");
